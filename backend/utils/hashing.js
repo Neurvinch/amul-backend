2a79859d0abc3b60683ef3149486f5e7a2245b99
Naveen pandian <npandian515@gmail.com>
2025-11-04 17:18:06 +0530

    teh hasing utisl are doen

--- backend/utils/hashing.js
+++ backend/utils/hashing.js
@@ -1,1 +1,12 @@
 const bcrypt = require('bcrypt');
+
+
+export const hashPassword = async (password) => {
+
+    const saltRounds = 10;
+
+    const hashedPasssword = await bcrypt.hash(password, saltRounds);
+
+    return hashedPasssword
+
+}
