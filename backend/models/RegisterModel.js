4d9161146e0cf020348a4a9a0f14c46b776b068b
Naveen pandian <npandian515@gmail.com>
2025-11-04 17:25:28 +0530

    registration done with module type

--- backend/models/RegisterModel.js
+++ backend/models/RegisterModel.js
@@ -1,12 +1,14 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
-const RegisterSchema  = new mongoose.Schema({
-    email: {type: String , required: true, unique: true},
-    password: {type: String , required: true},
+const RegisterSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    name: { type: String, required: true },
+    age: { type: Number, required: true },
+    bio: { type: String, required: false },
+  },
+  { timestamps: true }
+);
 
-    name: {type: String , required: true},
-    age: {type: Number , required: true},
-    bio: {type: String , required: false}
-})
-
-module.exports = mongoose.model("register" , RegisterSchema)
+export default mongoose.model("register", RegisterSchema);
